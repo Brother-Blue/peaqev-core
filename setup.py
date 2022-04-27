3f29c08715a825f6f147cac8b65b4c1f22d83860
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-04-27 23:01:12 +0200

    bump 0.0.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="peaqevcore",
-    version="0.0.18",
+    version="0.0.19",
     author="Magnus Eldén",
     description="Core types for peaqev car charging",
     url="https://github.com/elden1337/peaqev-core",
