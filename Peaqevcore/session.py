c7b11ecde887cba720bfda664ac5816f9dec5eb7
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-05-04 18:01:53 +0200

    adding session (for sessionprice at this time)

---
+++ Peaqevcore/session.py
@@ -0,0 +1,85 @@
+import time
+
+class PowerReading:
+    def __init__(self, price, power, td) -> None:
+        self._price = price
+        self._power = power
+        self._time_delta = td
+    
+    @property
+    def reading_integral(self):
+        return self.power/1000 * (self.time_delta/3600)
+
+    @property
+    def reading_cost(self):
+        return self.reading_integral * self.price
+
+    @property
+    def price(self):
+        return self._price
+
+    @property
+    def power(self):
+        return self._power
+
+    @property
+    def time_delta(self):
+        return self._time_delta
+
+
+class SessionPrice:
+    def __init__(self) -> None:
+        self._total_price = 0
+        self._price = 0
+        self._current_power = 0
+        self._total_power = 0
+        self._current_time = 0
+        self._time_delta = 0
+        self._readings = []
+    
+    @property
+    def total_power(self):
+        return round(self._total_power,5)
+
+    def terminate(self, mock_time:float=None):
+        print("called terminate")
+        self.update_power_reading(0, mock_time)
+        self._total_power = 0
+        self._total_price = 0
+        for i in self._readings:
+            self._total_power += i.reading_integral
+            self._total_price += i.reading_cost
+        
+            
+    def update_power_reading(self, power:float, mock_time:float=None):
+        self._set_delta(mock_time)
+        p = PowerReading(self._price, self._current_power, self._time_delta)
+        print(p.power, p.price, p.reading_cost, p.reading_integral)
+        self._readings.append(p)
+        self._current_power = power
+        
+    def update_price(self, price:float, mock_time:float=None):
+        if self._current_power > 0:
+            self.update_power_reading(
+                power=self._current_power, 
+                mock_time=mock_time
+                )
+        self._price = price
+
+    def _set_delta(self, mock_time:float=None) -> None:
+        now = time.time() if mock_time is None else mock_time
+        self._time_delta = (now - self._current_time)
+        self._current_time = now
+
+    @property
+    def readings(self) -> list:
+        return self._readings
+
+    @property
+    def total_price(self) -> float:
+        return self._total_price
+
+    @total_price.setter
+    def total_price(self, val):
+        self.total_price = val
+
---
+++ Peaqevcore/tests/test_session.py
@@ -0,0 +1,118 @@
+from ..session import SessionPrice
+import pytest
+
+def test_session_fluctuate():
+    s = SessionPrice()
+    timer = 1651607299
+    s._set_delta(timer)
+    s.update_price(0.6, timer)
+    s.update_power_reading(6000, timer)    
+    timer += 1200
+    s.update_price(0.3, timer)
+    s.update_power_reading(3000, timer)    
+    timer += 1200    
+    s.terminate(timer)
+    assert s.total_power == 3
+    assert round(s.total_price,4) == 1.5
+
+def test_session_fluctuate_tenfold():
+    s = SessionPrice()
+    timer = 1651607299
+    s._set_delta(timer)
+    s.update_price(6, timer)
+    s.update_power_reading(6000, timer)    
+    timer += 1200
+    s.update_price(3, timer)
+    s.update_power_reading(3000, timer)    
+    timer += 1200    
+    s.terminate(timer)
+    assert s.total_power == 3
+    assert round(s.total_price,4) == 15
+
+def test_session_split_price():
+    s = SessionPrice()
+    timer = 1651607299
+    s._set_delta(timer)
+    s.update_price(2, timer)
+    s.update_power_reading(2000, timer)
+    timer += 900
+    s.update_price(3, timer)
+    timer += 900
+    s.update_power_reading(1000, timer)
+    timer += 1800
+    s.terminate(timer)
+
+    assert s.total_power == 1.5
+    assert s.total_price == 4
+
+
+def test_session_full_hour():
+    s = SessionPrice()
+    timer = 1651607299
+    s._set_delta(timer)
+    s.update_price(1, timer)
+    s.update_power_reading(1000, timer)
+    timer += 3600
+    s.update_power_reading(1000, timer)
+    s.terminate(timer)
+    assert s.total_power == 1
+    assert s.total_price == 1
+
+def test_session_half_hour():
+    s = SessionPrice()
+    timer = 1651607299
+    s._set_delta(timer)
+    s.update_price(1, timer)
+    s.update_power_reading(1000, timer)
+    timer += 1800
+    s.update_power_reading(1000, timer)
+    s.terminate(timer)
+    assert s.total_power == 0.5
+    assert s.total_price == 0.5
+
+def test_session_with_zero_periods():
+    s = SessionPrice()
+    timer = 1651607299
+    s._set_delta(timer)
+    s.update_price(2, timer)
+    s.update_power_reading(4000, timer)
+    timer += 900
+     #2kr
+    s.update_power_reading(0, timer) 
+    timer += 900
+     #2kr
+    s.update_price(3, timer)
+    timer += 900
+     #2kr
+    s.update_power_reading(1000, timer)
+    timer += 1800
+     #3.5kr
+    s.terminate(timer)
+
+    assert s.total_power == 1.5
+    assert s.total_price == 3.5
+
+def test_session_with_zero_periods_price_update_in_between():
+    s = SessionPrice()
+    timer = 1651607299
+    s._set_delta(timer)
+    s.update_price(2, timer)
+    s.update_power_reading(4000, timer)
+    timer += 900
+     #2kr
+    s.update_price(3, timer)
+    timer += 900
+    #5kr
+    s.update_power_reading(0, timer) 
+    timer += 900
+     #5kr
+    s.update_power_reading(1000, timer)
+    timer += 1800
+     #6.5kr
+    s.terminate(timer)
+
+    assert s.total_power == 2.5
+    assert s.total_price == 6.5
+
+
+
