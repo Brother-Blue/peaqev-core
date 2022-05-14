4058048523b4534ddf891d55ca6e7286058b23bd
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-05-14 21:59:31 +0200

    alter_init

--- Peaqevcore/__init__.py
+++ Peaqevcore/__init__.py
@@ -2,4 +2,4 @@ from .Threshold import ThresholdBase as Threshold
 from .Chargecontroller import ChargeControllerBase as ChargeController
 from .Prediction import PredictionBase as Prediction
 from .hoursselection import Hoursselectionbase as Hoursselection
-from .session import SessionPrice
+from .session import SessionPrice as Session
