5564343f45b80a0af7cd810645ae392f1dc571d6
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-05-31 16:51:08 +0200

    average_kwh_price

--- Peaqevcore/hoursselection.py
+++ Peaqevcore/hoursselection.py
@@ -261,53 +261,79 @@ class Hoursselectionbase:
         return []
 
     def get_average_kwh_price(self, testhour:int = None):
-        if len(self._prices) > 0:
-            hour = datetime.now().hour if testhour is None else testhour
-            ret = {}
+        hour = datetime.now().hour if testhour is None else testhour
+        ret = {}
 
-            for h in self._dynamic_caution_hours:
-                if len(self._prices_tomorrow) > 0:
-                    if h < hour and len(self._prices_tomorrow) > 0:
-                        ret[h] = self._dynamic_caution_hours[h] * self._prices_tomorrow[h]
+        def _looper(h:int):
+            if h in self._dynamic_caution_hours:
+                    if self._prices_tomorrow is not None and len(self._prices_tomorrow) > 0:
+                        if h < hour and len(self._prices_tomorrow) > 0:
+                            ret[h] = self._dynamic_caution_hours[h] * self._prices_tomorrow[h]
+                    if h >= hour:
+                        ret[h] = self._dynamic_caution_hours[h] * self._prices[h]
+            elif h not in self._non_hours:
+                if h < hour and len(self._prices_tomorrow) > 0:
+                    ret[h] = self._prices_tomorrow[h]
                 if h >= hour:
-                    ret[h] = self._dynamic_caution_hours[h] * self._prices[h]
+                    ret[h] = self._prices[h]
 
-            for nh in self._non_hours:
-                ret[nh] = 0
+        if self.prices_tomorrow is None:
+            for h in range(hour,24):
+                _looper(h)
+        else:
+            for h in range(hour,(hour+24)):
+                h = h-24 if h > 23 else h
+                _looper(h)
+        
+        return round(sum(ret.values())/len(ret),2)
+        
+        
+        
+        # if len(self._prices) > 0:
+        #     hour = datetime.now().hour if testhour is None else testhour
+        #     ret = {}
 
-            for i in range(hour,24):
-                if i not in ret.keys():
-                    if len(self._prices_tomorrow) > 0:
-                        if i < hour and len(self._prices_tomorrow) > 0:
-                            ret[i] = self._prices_tomorrow[i]
-                    if i >= hour:
-                        ret[i] = self._prices[i]
+        #     for h in self._dynamic_caution_hours:
+        #         if len(self._prices_tomorrow) > 0:
+        #             if h < hour and len(self._prices_tomorrow) > 0:
+        #                 ret[h] = self._dynamic_caution_hours[h] * self._prices_tomorrow[h]
+        #         if h >= hour:
+        #             ret[h] = self._dynamic_caution_hours[h] * self._prices[h]
 
-            return round(sum(ret.values())/len(ret),2)
-        return "-"
+        #     for nh in self._non_hours:
+        #         ret[nh] = 0
+
+        #     for i in range(hour,24):
+        #         if i not in ret.keys():
+        #             if len(self._prices_tomorrow) > 0:
+        #                 if i < hour and len(self._prices_tomorrow) > 0:
+        #                     ret[i] = self._prices_tomorrow[i]
+        #             if i >= hour:
+        #                 ret[i] = self._prices[i]
+
+        #     return round(sum(ret.values())/len(ret),2)
+        # return "-"
 
     def get_total_charge(self, currentpeak:float, testhour:int = None) -> float:
         hour = datetime.now().hour if testhour is None else testhour
         ret = {}
 
+        def _looper(h:int):
+            if h in self._dynamic_caution_hours:
+                    ret[h] = self._dynamic_caution_hours[h] * currentpeak
+            elif h in self._non_hours:
+                ret[h] = 0
+            else:
+                ret[h] = currentpeak
+
         if self.prices_tomorrow is None:
             for h in range(hour,24):
-                if h in self._dynamic_caution_hours:
-                    ret[h] = self._dynamic_caution_hours[h] * currentpeak
-                elif h in self._non_hours:
-                    ret[h] = 0
-                else:
-                    ret[h] = currentpeak
+                _looper(h)
         else:
             for h in range(hour,(hour+24)):
                 h = h-24 if h > 23 else h
-                if h in self._dynamic_caution_hours:
-                    ret[h] = self._dynamic_caution_hours[h] * currentpeak
-                elif h in self._non_hours:
-                    ret[h] = 0
-                else:
-                    ret[h] = currentpeak
-            
+                _looper(h)
+        
         return round(sum(ret.values()),1)
 
     @staticmethod
@@ -317,6 +343,3 @@ class Hoursselectionbase:
             return ret
         except:
             return False
-
-
-
