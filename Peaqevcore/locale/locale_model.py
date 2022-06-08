4a8a9a55b15cd77cfaffadca6968e22e9d3ac562
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-06-08 16:59:31 +0200

    first localetype_test

--- Peaqevcore/locale/locale_model.py
+++ Peaqevcore/locale/locale_model.py
@@ -2,9 +2,9 @@ from dataclasses import dataclass
 from datetime import datetime, date, time
 from typing import List
 
-HOURLY = "Hourly"
-QUERYTYPE_BASICMAX = "bbfs"
-
+from .constants import (
+HOURLY
+)
 
 @dataclass(frozen=True)
 class Locale_Type:
@@ -23,40 +23,5 @@ class Locale_Type:
                     if now.hour in p["H"]:
                         return True
         return False
-
-
-
-
-#dag kl. 06-22 nov-mars                 106,25 kr/kW/mån
-#dag kl. 06-22 april-okt                50 kr/kW/mån
-#natt kl. 22-06 alla dagar hela året    0 kr/kW/mån
-
-@dataclass(frozen=True)
-class SE_Bjerke_Energi(Locale_Type):
-    observed_peak = QUERYTYPE_BASICMAX
-    charged_peak = QUERYTYPE_BASICMAX
-    free_charge_pattern = [
-        {
-            "M": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-            "D": [0, 1, 2, 3, 4, 5, 6],
-            "H": [22, 23, 0, 1, 2, 3, 4, 5]
-        }
-    ]
-
-"""
-Note, high load extra is added from 06-22 during november - march. 
-This does not affect the peak, but should in future updates be cause for forced non-/or caution-hours to lessen the cost for the consumer.
-"""
-
-#https://www.bjerke-energi.se/elnat/tariffer/effekttariff-fr-o-m-2022-02-01/
-
-
-#-----------------------------------------------------
-
-p = SE_Bjerke_Energi
-d = date(2005, 7, 14)
-t = time(22, 30)
-dt = datetime.combine(d, t)
-print(p.is_free_charge(p, mockdt=dt))
 
 
---
+++ Peaqevcore/tests/test_localetypes.py
@@ -0,0 +1,15 @@
+from datetime import datetime, date, time
+import pytest
+from ..locale.types.sweden import SE_Bjerke_Energi
+
+def test_SE_Bjerke_Energi():
+    p = SE_Bjerke_Energi
+    d = date(2005, 7, 14)
+    t = time(22, 30)
+    dt = datetime.combine(d, t)
+    assert p.is_free_charge(p, mockdt=dt) is True
+    t2 = time(15,00)
+    dt2 = datetime.combine(d, t2)
+    assert p.is_free_charge(p, mockdt=dt2) is False
+
+
