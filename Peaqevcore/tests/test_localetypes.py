3c5e9712feac55c7afda340084f389e0d24a6ab8
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-06-12 15:12:32 +0200

    add tests

--- Peaqevcore/tests/test_localetypes.py
+++ Peaqevcore/tests/test_localetypes.py
@@ -1,5 +1,7 @@
 from datetime import datetime, date, time
 import pytest
+
+from ..locale.querytypes import QUERYTYPE_AVERAGEOFTHREEDAYS, QUERYTYPE_AVERAGEOFTHREEHOURS, QUERYTYPES
 from ..country.sweden import SE_Bjerke_Energi, SE_Gothenburg
 
 def test_SE_Bjerke_Energi():
@@ -12,12 +14,22 @@ def test_SE_Bjerke_Energi():
     dt2 = datetime.combine(d, t2)
     assert p.free_charge(p, mockdt=dt2) is False
 
+def test_generic_querytype_avg_threedays():
+    p1 = QUERYTYPES[QUERYTYPE_AVERAGEOFTHREEDAYS]
+    d11 = date(2022, 7, 14)
+    t11 = time(20, 30)
+    dt11 = datetime.combine(d11, t11)
+    p1.try_update(newval=1.2, dt=dt11)
+    d21 = date(2022, 7, 14)
+    t21 = time(21, 30)
+    dt21 = datetime.combine(d21, t21)
+    p1.try_update(newval=2, dt=dt21)
+    assert len(p1._peaks.p) == 1
 
 def test_SE_Gothenburg():
     p = SE_Gothenburg
     assert p.converted
     assert p.free_charge(p) is False
-    assert p.query_model.observed_peak == 0
     d1 = date(2022, 7, 14)
     t = time(22, 30)
     dt1 = datetime.combine(d1, t)
@@ -34,7 +46,16 @@ def test_SE_Gothenburg():
     d4 = date(2022, 7, 19)
     dt4 = datetime.combine(d4, t)
     p.query_model.try_update(newval=1.5, dt=dt4)
-    print(p.query_model._peaks)
-    print(p.query_model.peaks)
+    assert p.query_model.observed_peak > 0
 
-
+def test_generic_querytype_avg_threehours():
+    p2 = QUERYTYPES[QUERYTYPE_AVERAGEOFTHREEHOURS]
+    d1 = date(2022, 7, 14)
+    t = time(20, 30)
+    dt1 = datetime.combine(d1, t)
+    p2.try_update(newval=1.2, dt=dt1)
+    d2 = date(2022, 7, 14)
+    t2 = time(21, 30)
+    dt2 = datetime.combine(d2, t2)
+    p2.try_update(newval=2, dt=dt2)
+    assert len(p2._peaks.p) == 2
