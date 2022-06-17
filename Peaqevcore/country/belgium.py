f284f6d3d18469377aaf6a297a48da9c263c62d8
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-06-17 16:05:21 +0200

    finalize localetypes.

--- Peaqevcore/country/belgium.py
+++ Peaqevcore/country/belgium.py
@@ -1,5 +1,5 @@
+from ..locale.querytypes.querytypes import QUERYTYPES
 from ..locale.querytypes.const import (QUARTER_HOURLY, QUERYTYPE_BASICMAX)
-
 from dataclasses import dataclass
 from ..locale.locale_model import Locale_Type
 
@@ -8,6 +8,8 @@ from ..locale.locale_model import Locale_Type
 class VregBelgium(Locale_Type):
     observed_peak = QUERYTYPE_BASICMAX
     charged_peak = QUERYTYPE_BASICMAX
+    converted = True
+    query_model = QUERYTYPES[QUERYTYPE_BASICMAX]
     #TODO: to be decided. Should charged_peak be turned into the real charged peak, ie the average of the months in a year? could be issues with the long term stats there and it won't help peaq in any way.
     peak_cycle = QUARTER_HOURLY
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="peaqevcore",
-    version="1.0.17",
+    version="1.0.19",
     author="Magnus Eldén",
     description="Core types for peaqev car charging",
     url="https://github.com/elden1337/peaqev-core",
