032e81261ef520ca11892d03ca1dace090ee6dac
Christian O'Neill <oneill.christian97@gmail.com>
2022-06-21 23:15:54 +0200

    Refactor and update naming in hoursselection
    Made some methods more pythonic with list comprehension, removed some duplicate code

--- Peaqevcore/hoursselection.py
+++ Peaqevcore/hoursselection.py
@@ -1,4 +1,5 @@
 import collections
+from operator import mod
 import statistics as stat
 from datetime import datetime
 from .Models import (
@@ -107,10 +108,10 @@ class Hoursselectionbase:
     def min_price(self) -> float:
         return self._min_price
 
-    def update(self, testhour:int = None):
+    def update(self, test_hour: int=None):
         hours_today = self._update_internal(self.prices)
         hours_tomorrow = self._update_internal(self.prices_tomorrow)
-        hour = datetime.now().hour if testhour is None else testhour
+        hour = datetime.now().hour if test_hour is None else test_hour
 
         self.non_hours = []
         self.caution_hours = []
@@ -132,24 +133,24 @@ class Hoursselectionbase:
     def _update_internal(self, prices) -> HourObject:
         ret = HourObject([], [], {})
         if prices is not None and len(prices) > 1:
-            pricedict = self._create_dict(prices)
-            normalized_pricedict = self._create_dict(self._normalize_prices(prices))
+            price_dict = self._create_dict(prices)
+            normalized_price_dict = self._create_dict(self._normalize_prices(prices))
             
             """
             Curve is too flat if stdev is <= 0.05. 
             If so we don't do any specific non or caution-hours based on pricing.
             """
             if stat.stdev(prices) > 0.05:
-                prices_ranked = self._rank_prices(pricedict, normalized_pricedict)
+                prices_ranked = self._rank_prices(price_dict, normalized_price_dict)
                 ready_hours = self._determine_hours(prices_ranked, prices)
             else:
                 ready_hours = ret
             if self._absolute_top_price is not None:
-                ret = self._add_expensive_non_hours(pricedict, ready_hours)
+                ret = self._add_expensive_non_hours(price_dict, ready_hours)
             else: 
                 ret = ready_hours
             if self._min_price > 0:
-                ret = self._remove_cheap_hours(pricedict, ret)
+                ret = self._remove_cheap_hours(price_dict, ret)
         return ret
         
     def _normalize_prices(self, prices) -> list:
@@ -159,39 +160,39 @@ class Hoursselectionbase:
             ret.append(p/min_price)
         return ret
 
-    def _remove_cheap_hours(self, hourdict: dict, hours: HourObject) -> HourObject:
-        lst = (h for h in hourdict if hourdict[h] < self._min_price)
-        for h in lst:
-            if h in hours.nh:
-                hours.nh.remove(h)
-            elif h in hours.ch:
-                hours.ch.remove(h)
-                hours.dyn_ch.pop(h)    
-        return hours
+    def _remove_cheap_hours(self, hour_dict: dict, cheap_hours: HourObject) -> HourObject:
+        hours = (hour for hour in hour_dict if hour_dict.get(hour) is not None and hour_dict.get(hour) < self._min_price)
+        for hour in hours:
+            if hour in cheap_hours.nh:
+                cheap_hours.nh.remove(hour)
+            elif hour in cheap_hours.ch:
+                cheap_hours.ch.remove(cheap_hours.dyn_ch.pop(hour))
+        return cheap_hours
 
-    def _add_expensive_non_hours(self, hourdict: dict, readyhours:HourObject) -> HourObject:
-        lst = (h for h in hourdict if hourdict[h] >= self._absolute_top_price)
-        for h in lst:
-            if h not in readyhours.nh:
-                readyhours.nh.append(h)
-                if h in readyhours.ch:
-                    readyhours.ch.remove(h)
-                if h in readyhours.dyn_ch.keys():
-                    readyhours.dyn_ch.pop(h)
-        readyhours.nh.sort()
-        return readyhours
+    def _add_expensive_non_hours(self, hour_dict: dict, ready_hours: HourObject) -> HourObject:
+        hours = (hour for hour in hour_dict if hour_dict.get(hour) is not None and hour_dict.get(hour) >= self._absolute_top_price)
+        for hour in hours:
+            if hour not in ready_hours.nh:
+                ready_hours.nh.append(hour)
+                if hour in ready_hours.ch:
+                    ready_hours.ch.remove(hour)
+                if hour in ready_hours.dyn_ch.keys():
+                    ready_hours.dyn_ch.pop(hour)
+        ready_hours.nh.sort()
+        return ready_hours
 
-    def _rank_prices(self, hourdict: dict, normalized_hourdict: dict) -> dict:
+    def _rank_prices(self, hour_dict: dict, normalized_hour_dict: dict) -> dict:
         ret = {}
-        _maxval = max(hourdict.values())
-        _max_normalized = max(normalized_hourdict.values())
-        peaqstdev = _maxval/abs(_max_normalized/stat.stdev(normalized_hourdict.values()))
-        if peaqstdev < min(hourdict.values()):
-            peaqstdev = peaqstdev + min(hourdict.values())
-        for key in hourdict:
-            if hourdict[key] > peaqstdev:
-                _permax = round(hourdict[key] / _maxval, 2)
-                ret[key] = {"val": hourdict[key], "permax": _permax}
+        maxval = max(hour_dict.values())
+        minval = min(hour_dict.values())
+        max_normalized = max(normalized_hour_dict.values())
+        peaq_stdev = maxval/abs(max_normalized/stat.stdev(normalized_hour_dict.values()))
+        if peaq_stdev < minval:
+            peaq_stdev = peaq_stdev + minval
+        for key in hour_dict:
+            if hour_dict.get(key) > peaq_stdev:
+                _permax = round(hour_dict[key] / maxval, 2)
+                ret[key] = {"val": hour_dict[key], "permax": _permax}
         return self._discard_excessive_hours(ret)
 
     def _discard_excessive_hours(self, hours: dict):
@@ -199,14 +200,12 @@ class Hoursselectionbase:
         if len(hours) < 20:
             return hours
         while len(hours) >= 20:
-            to_pop = dict(sorted(hours.items(), key=lambda item: item[1]['val']))    
-            hours.pop(list(to_pop.keys())[0])
+            to_pop = dict(sorted(hours.items(), key=lambda _, value: value.get('val')))    
+            hours.pop(to_pop.keys()[0])
         return hours
 
-    def _create_dict(self, input: list):
-        ret = {}
-        for idx, val in enumerate(input):
-            ret[idx] = val
+    def _create_dict(self, arr_input: list):
+        ret = {idx: val for idx, val in enumerate(arr_input)}
         try:
             assert len(ret) == 24
         except Exception:
@@ -214,26 +213,24 @@ class Hoursselectionbase:
         return ret
 
     def _determine_hours(self, price_list: dict, prices: list) -> HourObject:
-        _nh = []
-        _dyn_ch = {}
-        _ch = []
+        nh = []
+        dyn_ch = {}
+        ch = []
         for p in price_list:
-            _permax = round(abs(price_list[p]["permax"] - 1), 2)
+            per_max = round(abs(price_list.get(p).get("permax") - 1), 2)
             if self._cautionhour_type == CAUTIONHOURTYPE[CAUTIONHOURTYPE_SUAVE]:
-                _permax += 0.15
+                per_max += 0.15
             elif self._cautionhour_type == CAUTIONHOURTYPE[CAUTIONHOURTYPE_INTERMEDIATE]:
-                _permax += 0.05
-            if float(price_list[p]["permax"]) <= self._cautionhour_type:
-                _ch.append(p)
-                _dyn_ch[p] = round(_permax,2)
-            elif float(price_list[p]["val"]) <= (sum(prices)/len(prices)):
-                _ch.append(p)
-                _dyn_ch[p] = round(_permax,2)
+                per_max += 0.05
+            if float(price_list.get(p).get("permax")) <= self._cautionhour_type or \
+               float(price_list.get(p).get("val")) <= (sum(prices)/len(prices)):
+                ch.append(p)
+                dyn_ch[p] = round(per_max,2)
             else:
-                _nh.append(p)
-        return HourObject(_nh, _ch, _dyn_ch)
+                nh.append(p)
+        return HourObject(nh, ch, dyn_ch)
     
-    def _convert_none_list(self, lst:list) -> list:
+    def _convert_none_list(self, lst: list) -> list:
         try:
             ret = []
             for l in lst:
@@ -243,76 +240,74 @@ class Hoursselectionbase:
         except:
             return self._make_array_from_empty(lst)
 
-    def _make_array_from_empty(self, input) -> list:
-        array = input.split(",")
-        list = [p for p in array if len(p) > 0]
+    def _make_array_from_empty(self, arr_input) -> list:
+        arr = [p for p in arr_input.split(",") if len(p) > 0]
         ret = []
-        if len(list) > 24:
+        if len(arr) > 24:
             try:
-                for l in list:
+                for l in arr:
                     parsed_item = Hoursselectionbase._try_parse(l, float)
                     if not parsed_item:
                         parsed_item = Hoursselectionbase._try_parse(l, int)
                     assert type(parsed_item) is float or type(parsed_item) is int
                     ret.append(parsed_item)
                 return ret
-            except:
+            except ValueError:
                 return []
         return []
 
-    def get_average_kwh_price(self, testhour:int = None):
-        hour = datetime.now().hour if testhour is None else testhour
+    def get_average_kwh_price(self, test_hour: int=None):
+        hour = datetime.now().hour if test_hour is None else test_hour
         ret = {}
 
-        def _looper(h:int):
-            if h in self._dynamic_caution_hours:
-                    if self._prices_tomorrow is not None and len(self._prices_tomorrow) > 0:
-                        if h < hour and len(self._prices_tomorrow) > 0:
-                            ret[h] = self._dynamic_caution_hours[h] * self._prices_tomorrow[h]
-                    if h >= hour:
-                        ret[h] = self._dynamic_caution_hours[h] * self._prices[h]
-            elif h not in self._non_hours:
-                if h < hour and len(self._prices_tomorrow) > 0:
-                    ret[h] = self._prices_tomorrow[h]
-                if h >= hour:
-                    ret[h] = self._prices[h]
+        def _looper(_hour: int):
+            if _hour in self._dynamic_caution_hours:
+                    if self._prices_tomorrow is not None:
+                        if _hour < hour:
+                            ret[_hour] = self._dynamic_caution_hours.get(_hour) * self._prices_tomorrow[_hour]
+                    if _hour >= hour:
+                        ret[_hour] = self._dynamic_caution_hours.get(_hour) * self._prices[_hour]
+            elif _hour not in self._non_hours:
+                ret[_hour] = self._prices_tomorrow[_hour] \
+                    if _hour < hour and self._prices_tomorrow \
+                    else self._prices[_hour]
 
         if self.prices_tomorrow is None:
-            for h in range(hour,24):
+            for h in range(hour, 24):
                 _looper(h)
         else:
-            for h in range(hour,(hour+24)):
-                h = h-24 if h > 23 else h
+            for h in range(hour, (hour+24)):
+                h = h % 24
                 _looper(h)
         
-        return round(sum(ret.values())/len(ret),2)
+        return round(sum(ret.values())/len(ret), 2)
         
-    def get_total_charge(self, currentpeak:float, testhour:int = None) -> float:
-        hour = datetime.now().hour if testhour is None else testhour
+    def get_total_charge(self, current_peak:float, test_hour:int = None) -> float:
+        hour = datetime.now().hour if test_hour is None else test_hour
         ret = {}
 
-        def _looper(h:int):
-            if h in self._dynamic_caution_hours:
-                    ret[h] = self._dynamic_caution_hours[h] * currentpeak
-            elif h in self._non_hours:
-                ret[h] = 0
+        def _looper(_hour: int):
+            if _hour in self._dynamic_caution_hours:
+                    ret[_hour] = self._dynamic_caution_hours.get(_hour) * current_peak
+            elif _hour in self._non_hours:
+                ret[_hour] = 0
             else:
-                ret[h] = currentpeak
+                ret[_hour] = current_peak
 
         if self.prices_tomorrow is None:
-            for h in range(hour,24):
+            for h in range(hour, 24):
                 _looper(h)
         else:
-            for h in range(hour,(hour+24)):
-                h = h-24 if h > 23 else h
+            for h in range(hour, (hour+24)):
+                h = h % 24
                 _looper(h)
         
-        return round(sum(ret.values()),1)
+        return round(sum(ret.values()), 1)
 
     @staticmethod
-    def _try_parse(input:str, parsetype:type):
+    def _try_parse(val: str, parse_type: type):
         try:
-            ret = parsetype(input)
+            ret = parse_type(val)
             return ret
-        except:
+        except ValueError:
             return False
