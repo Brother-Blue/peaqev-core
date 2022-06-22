3f349c919b81ae81df08926b9b2cd8e50b6bee58
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-06-22 12:37:53 +0200

    2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="peaqevcore",
-    version="2.0.0",
+    version="2.0.1",
     author="Magnus Eldén",
     description="Core types for peaqev car charging",
     url="https://github.com/elden1337/peaqev-core",
